5571a078870bf6831f6fc6e92b68af6035b9f4ae
Enz0lanterne <79151100+Enz0lanterne@users.noreply.github.com>
2023-11-08 19:20:45 +0100

    v. 1.2.0 (Release)

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ import keyboard
 from tkinter import Tk, Label, Entry
 from tkinter import ttk
 from pynput.mouse import Listener
+from pynput import keyboard
+from pynput.keyboard import Key
+import os
+import json
 
-driver = webdriver.Chrome(executable_path=r'assets\chromedriver.exe')
+# Variables
+used_words = list()
+focused_letters = str()
 
-correct_words = []
-used_words = []
-focused_letters = ''
-
+# Json
+with open(r"assets\settings.json", "r") as j: 
+    settings = json.load(j)
+    
+# Functions
 def join_room():
     roomcode = url_entry.get()
     driver.get(f"https://jklm.fun/{roomcode}")
@@ -22,7 +29,7 @@ def join_room():
     ok_button.click()
 
     driver.maximize_window()
-
+        
 def find_maximum(lst,start=0,max_word=''):
     if start == len(lst):
         return max_word
@@ -30,40 +37,46 @@ def find_maximum(lst,start=0,max_word=''):
         max_word = lst[start]
     return find_maximum(lst, start + 1, max_word)
 
-syl_x = 0
-syl_y = 0
-bar_x = 0
-bar_y = 0
+# A Optimiser #
+
+def update_json(settings):
+    with open(r"assets\settings.json", "w") as j: 
+        json.dump(settings, j, indent=4)
 
 def detect_click_bomb():
-    global syl_x, syl_y
 
     def on_click(x, y, button, pressed):
-        global syl_x, syl_y
         print(x, y)
-        syl_x = x
-        syl_y = y
+        for k in settings.keys():
+            if k == "bomb_position":
+                settings[k] = [x, y]
+                update_json(settings)
         return False
         
     with Listener(on_click=on_click) as listener:
         listener.join()
-        
+            
 def detect_click_input():
-    global bar_x, bar_y
     
     def on_click(x, y, button, pressed):
-        global bar_x, bar_y
         print(x, y)
-        bar_x = x
-        bar_y = y
+        for k in settings.keys():
+            if k == "input_position":
+                settings[k] = [x, y]
+                update_json(settings)
         return False
         
     with Listener(on_click=on_click) as listener:
         listener.join()
-        
+###
 
-def process(e):
-    pyautogui.moveTo(syl_x, syl_y)
+def process():
+    
+    global used_words
+    
+    for k, v in settings.items():
+        if k == "bomb_position":
+            pyautogui.moveTo(v[0], v[1])
     pyautogui.click(clicks=2)
     pyautogui.hotkey('ctrl', 'c')
 
@@ -88,67 +101,84 @@ def process(e):
 
     if longest_word:
         print(longest_word)
-        pyautogui.click(bar_x, bar_y)
+        for k, v in settings.items():
+            if k == "input_position":
+                pyautogui.click(v[0], v[1])
         for i in longest_word:
             sleep(random.uniform(0.02, 0.12))
             keyboard.write(i)
         used_words.append(longest_word)
     else:
         print("Aucun mot trouvé pour les lettres spécifiées.")
-
+        
 def reset_used_words():
     global used_words
     used_words = []
-
-
-    keyboard.on_press_key('0', process)
-
+    
 def update_focused_letters(*args):
     global focused_letters
     focused_letters = letters_entry.get()
 
+# Updater avec keybind
+keyboard.on_press_key('0', process)
 
+# Selenium
+driver = webdriver.Chrome(executable_path=r'assets\chromedriver.exe')
+
+# Tkinter
 root = Tk()
 root.title("Guest is too good - Bomb Party")
-root.geometry("300x175")
+root.geometry("400x175")
 root.resizable(False, False)
-
 root.iconbitmap(r"assets\bomb.ico")
 
 code_label = Label(root, text="Code de partie : ")
 code_label.place(x=0, y=18)
 
 url_entry = Entry(root)
-url_entry.place(x=90, y=20)
+url_entry.place(x=120, y=20)
 
 join_button = ttk.Button(root, text="Rejoindre", command=join_room)
-join_button.place(x=215, y=18)
+join_button.place(x=287, y=18)
 
 get_syll_button = ttk.Button(root, text="Tricher", command=process)
-get_syll_button.place(x=75, y=45)
+get_syll_button.place(x=100, y=45)
 
 reset_button = ttk.Button(root, text="Réinitialiser", command=reset_used_words)
-reset_button.place(x=150, y=45)
+reset_button.place(x=200, y=45)
 
 syll_label = Label(root, text="Syllabe : ")
-syll_label.place(x=125, y=75)
+syll_label.place(x=167, y=75)
 
 letters_label = Label(root, text="Lettres prioritaires :")
 letters_label.place(x=0, y=98)
 
 letters_entry = Entry(root)
-letters_entry.place(x=110, y=100)
+letters_entry.place(x=147, y=100)
 
 bomb_config = ttk.Button(root, text="Bomb Config", command=detect_click_bomb)
-bomb_config.place(x=65, y=125)
+bomb_config.place(x=87, y=125)
+
+code_label = Label(root, text="")
+code_label.place(x=0, y=18)
 
 input_config = ttk.Button(root, text="Answer Input Config", command=detect_click_input)
-input_config.place(x=150, y=125)
+input_config.place(x=200, y=125)
 
 letters_entry.bind("<KeyRelease>", update_focused_letters)
 
 root.attributes('-topmost', 1)
 
-root.mainloop()
 
-driver.quit()
+def main():
+
+    # Start code
+    root.mainloop()
+
+    # End
+    driver.quit()
+    os.system("quit")
+
+# Start code
+if __name__ == "__main__":
+    main()
